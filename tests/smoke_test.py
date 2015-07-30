7c2df30bcd35a3e5c3b1d93d3468a8e87d098f8d
Wesley Baugh <wesley@bwbaugh.com>
2015-07-30 13:40:47 -0700

    Set up tox for testing

---
+++ tests/smoke_test.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import rotal
+
+
+def test_import_smoke():
+    """Temporary smoke test until real tests are written."""
+    assert rotal
